61e08accc85e5bb4c0b609cbe3399276c2d1e805
lime48 <limelime588@gmail.com>
2025-11-06 02:12:00 -0800

    Add files via upload

---
+++ collectdomainsgit.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#grep -E -o "https?://[A-Za-z0-9_]+\.[a-zA-Z]{,2}(/[a-zA-Z0-9]+)";\
+#grep -E -o "[A-Za-z0-9_]+\.(com|org|net)";\ 
+#grep -E -o "https?://(www\.)?[A-Za-z0-9_]+\.(com|org|net)(/[0-9A-Za-z/_]+)?"
+#Author : Limeeee48
+#poC
+
+BoldWhite="\e[1;37m"
+EndLine="\e[0m"
+sucessColor="\e[32m"
+ErrorColor="\e[31m"
+first_url="(https?://)?(www\.)?[A-Za-z0-9_]+\.(com|net|org|gov)(/[0-9A-Za-z/_]+)?"
+#first_url="https?://(www\.)?[A-Za-z0-9_]+\.(com|net|org|gov)(/[0-9A-Za-z/_]+)?"
+dll_="^[a-zA-Z0-9_]+\.dll$"
+banner()
+{
+echo -e """${sucessColor}
+⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣀⣀⣀⣀⣀⣀⣀⣀⠀⠀⠀⠀⠀
+⠀⠀⠀⠀⠀⢀⣀⣠⣤⣴⣶⡶⢿⣿⣿⣿⠿⠿⠿⠿⠟⠛⢋⣁⣤⡴⠂⣠⡆⠀
+⠀⠀⠀⠀⠈⠙⠻⢿⣿⣿⣿⣶⣤⣤⣤⣤⣤⣴⣶⣶⣿⣿⣿⡿⠋⣠⣾⣿⠁⠀
+⠀⠀⠀⠀⠀⢀⣴⣤⣄⡉⠛⠻⠿⠿⣿⣿⣿⣿⡿⠿⠟⠋⣁⣤⣾⣿⣿⣿⠀⠀
+⠀⠀⠀⠀⣠⣾⣿⣿⣿⣿⣿⣶⣶⣤⣤⣤⣤⣤⣤⣶⣾⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠀⠀⣰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡇⠀
+⠀⠀⢰⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠁⠀
+⠀⢀⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠇⢸⡟⢸⡟⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⢿⣷⡿⢿⡿⠁⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⡟⢁⣴⠟⢀⣾⠃⠀⠀⠀
+⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠛⣉⣿⠿⣿⣶⡟⠁⠀⠀⠀⠀
+⠀⠀⢿⣿⣿⣿⣿⣿⣿⣿⣿⡿⠿⠛⣿⣏⣸⡿⢿⣯⣠⣴⠿⠋⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⣿⣿⣿⣿⠿⠶⣾⣿⣉⣡⣤⣿⠿⠛⠁⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⢸⣿⣿⣿⣿⡿⠿⠿⠿⠶⠾⠛⠛⠛⠉⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+⠀⠀⠈⠉⠉⠉⠀⠀⠀⠀⠀⠀⠀⠀${BoldWhite}Author : LIME⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀${EndLine}"""
+
+}
+getdomains() 
+{
+
+if [ $# -eq 0 ] ; then 
+	echo -e "${ErrorColor}Usage : collectdomains <EXE ,DLL, Script>${EndLine}";exit 1 
+else 
+	if [ -f "$1" ] ; then
+		tmpfile=$(mktemp)  
+		domains=$(mktemp)
+		strings $1 > $tmpfile 
+		cat $tmpfile | grep -E $first_url -o > $domains
+		if [ -n "$(cat $domains)" ] ; then 
+			#echo -e "${BoldWhite}============Domains============\n${EndLine}"
+			cat $domains | sort -u
+			rm -rf $tmpfile $domains
+		else 
+			echo -e "${ErrorColor}No Domains Found .${EndLine}"
+		fi
+	else 
+		echo -e "${ErrorColor}[-] File $1 Not Found${EndLine}"
+	fi
+fi
+}
+banner
+getdomains $1
+
